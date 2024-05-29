6b4e23b24f78cb3fd2562225e4fd982d086fc3a5
Nina <morville5audrey@gmail.com>
2024-05-29 22:39:51 +0200

    api call and data mapping

--- src/pages/accueil/Accueil.jsx
+++ src/pages/accueil/Accueil.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
-import axios from "axios";
-import { useQueryClient,useQuery, QueryClientProvider } from '@tanstack/react-query';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
+
+const base_url = "http://localhost:3001/users/";
+
+
+
+/*
 const fetchApi = async () =>{
-    const res = await fetch ("https://nina55555.github.io/node-video-api/")
+    const res = await fetch ("http://localhost:3001/users/")
     return res.json()
 }
-    
-
+  */ 
 
 const Accueil = () => {
 
+    const [data, setData] = useState([])
+    useEffect(()=> {
+        //axios.get('http://localhost:5980/runways/')
+        axios.get('http://localhost:3001/users/')
+        .then(res => setData(res.data))
+        .catch(err=> console.log(err))
+    },[] )
 
+
+    return (
+        <div>
+            <h1>accueil</h1>
+            {
+                data.map((d,i)=> {
+                    return <p key={i}>{d.author} </p>
+                })
+            }
+
+        </div>
+    );
+   
+};
+
+export default Accueil;
 
     /*
     //acces au client
@@ -38,14 +65,3 @@ const Accueil = () => {
         })
     }
      */
-    return (
-        <div>
-            <h1>accueil</h1>
-            {fetchApi}
-
-        </div>
-    );
-   
-};
-
-export default Accueil;
